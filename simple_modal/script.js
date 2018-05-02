7744d460d68e225719cc98fca94fd4d347ae525c
Yask00 <jasstreb@abv.bg>
2018-05-02 10:24:14 +0300

    cleared simple modal demo

--- simple_modal/script.js
+++ simple_modal/script.js
@@ -1,6 +1,17 @@
-let close = document.getElementById('close-modal');
+let close = document.getElementsByClassName('close-modal');
+let overlay = document.getElementsByClassName('overlay');
+let openModal = document.getElementsByTagName('button');
+// close.addEventListener('click', () => {
+//   overlay.style.display = `none`;
+// });
 
-close.addEventListener('click', () => {
-  let overlay = document.getElementById('overlay');
-  overlay.style.display = `none`;
+overlay[0].addEventListener('click', () => {
+  if(event.target.classList.contains('modal')){
+    return false;
+  }
+  overlay[0].classList.add('hidden-element');
+});
+
+openModal[0].addEventListener('click', () => {
+  overlay[0].classList.remove('hidden-element');
 });

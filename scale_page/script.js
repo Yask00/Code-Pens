823b710c65b7605ec8180dd884e607043bc71e54
Yasen Trayanov <jasstreb@abv.bg>
2019-01-09 21:17:17 +0200

    scaling home page content from center to whole page

---
+++ scale_page/script.js
@@ -0,0 +1,6 @@
+(function() {
+    setTimeout(() => {
+        document.getElementById("scalable").style.visibility = "visible";
+        document.getElementById("scalable").style.transform = "scale(2)";
+    }, 500)
+})();
